41227f80912a342540c347ed529ae76cc82453a1
liyang40 <liyang40@newhope.cn>
2020-06-24 11:31:07 +0800

    feat: 编写好销贷授信及支用流程页面

--- widget/html/app/index.js
+++ widget/html/app/index.js
@@ -1802,12 +1802,79 @@ var navigationBarWhite = {
   }]
 }; // 绿色底导航
 
+var navigationBarGreen = {
+  hideBackButton: false,
+  background: themeMainColor,
+  color: '#fff',
+  fontSize: 18,
+  fontWeight: 'bold',
+  leftButtons: [{
+    text: '',
+    color: '#fff',
+    iconPath: 'widget://image/back_white_big.png'
+  }]
+}; // router page 配置信息
+
 var routerMapConfig = {
-  // 好销贷产品申请
+  // 好销贷授信申请
   hxd_apply: {
     name: 'hxd_apply',
     title: '产品介绍',
     url: 'widget://html/hxd_apply/index.html',
+    bgColor: '#fff',
+    reload: true,
+    navigationBar: navigationBarWhite
+  },
+  // 好销贷授信申请补充企业信息
+  hxd_a_supply: {
+    name: 'hxd_a_supply',
+    title: '补充企业信息',
+    url: 'widget://html/hxd_a_supply/index.html',
+    bgColor: '#fff',
+    reload: true,
+    navigationBar: navigationBarWhite
+  },
+  // 好销贷授信申请成功/失败
+  hxd_a_success: {
+    name: 'hxd_a_success',
+    title: '产品开通',
+    url: 'widget://html/hxd_a_success/index.html',
+    bgColor: '#fff',
+    reload: true,
+    navigationBar: navigationBarGreen
+  },
+  // 好销贷产品详情
+  hxd_product_detail: {
+    name: 'hxd_product_detail',
+    title: '产品详情',
+    url: 'widget://html/hxd_product_detail/index.html',
+    bgColor: '#fff',
+    reload: true,
+    navigationBar: navigationBarWhite
+  },
+  // 好销贷额度变化详情
+  hxd_quota: {
+    name: 'hxd_quota',
+    title: '额度变化详情',
+    url: 'widget://html/hxd_quota/index.html',
+    bgColor: '#fff',
+    reload: true,
+    navigationBar: navigationBarWhite
+  },
+  // 好销贷用款申请
+  hxd_u_apply: {
+    name: 'hxd_u_apply',
+    title: '申请用款',
+    url: 'widget://html/hxd_u_apply/index.html',
+    bgColor: '#fff',
+    reload: true,
+    navigationBar: navigationBarWhite
+  },
+  // 好销贷用款确认
+  hxd_u_confirm: {
+    name: 'hxd_u_confirm',
+    title: '申请用款',
+    url: 'widget://html/hxd_u_confirm/index.html',
     bgColor: '#fff',
     reload: true,
     navigationBar: navigationBarWhite
@@ -1883,7 +1950,7 @@ var App = /*#__PURE__*/function () {
         // openDanbaoKaitong({step: 0, creditStatus: 2})
         // return
         Router$2.openPage({
-          key: 'hxd_apply'
+          key: 'hxd_u_confirm'
         });
         return;
       } else {
---
+++ widget/html/hxd_u_apply/index.js
@@ -0,0 +1,39 @@
+var page = new Vue({
+  el: '#app',
+  data: {
+    isWarning: true,
+    orders: [{
+      title: "成都阳光农业服务有限公司1",
+      can: "120000",
+      left: "66000",
+      date: "2020-06-12",
+      orderId: "77889900022009988",
+      isSelected: true
+    }, {
+      title: "成都阳光农业服务有限公司2",
+      can: "120000",
+      left: "66000",
+      date: "2020-06-12",
+      orderId: "77889900000992288",
+      isSelected: true
+    }, {
+      title: "成都阳光农业服务有限公司3",
+      can: "120000",
+      left: "66000",
+      date: "2020-06-12",
+      orderId: "73378899000009988",
+      isSelected: false
+    }]
+  },
+  methods: {}
+});
+
+apiready = function apiready() {
+  api.addEventListener({
+    name: 'navitembtn'
+  }, function (ret, err) {
+    if (ret.type === 'left') {
+      api.closeWin();
+    }
+  }); // alert(Vue)
+};
